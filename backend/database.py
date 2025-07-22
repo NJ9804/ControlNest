4272eb5b8e20ac7aa0487dbb0a463181db79a39b
Nandana J <jnandana04@gmail.com>
2025-07-22 17:54:47 +0530

    db

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://data_owner:npg_Br59HfkJdwXv@ep-raspy-mountain-a15lgumk-pooler.ap-southeast-1.aws.neon.tech/data?sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_T5hAZDOG8HJl@ep-soft-poetry-a1fj80x4-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
